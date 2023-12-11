060bd21ef40187e4b9beacdf2ab98edae5705b45
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2023-12-11 17:49:24 +0300

    Add all placeholders for backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse 
+import json
 app = FastAPI()
 origins = [
     "http://localhost",
@@ -16,9 +17,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 class SendHelloWorld:
-    #data: str
     def __init__(self):
         self.data = "Hello world"
+
+class LoginResponce:
+    def __init__(self, success: bool) -> None:
+        self.data = {'status':'OK' if success else 'ERR'}
+
+class Course:
+    def __init__(self,name: str,desc: str) -> None:
+        self.name = name
+        self.description = desc
+
+class CoursesList:
+    def __init__(self,courses: list[Course]) -> None:
+        self.data = {"courses":courses}
 @app.get("/")
 async def main():
     return FileResponse('placeholder.html')
+
+@app.post("/api/login")
+async def main():
+    return LoginResponce(True)
+
+@app.post('/api/get_courses')
+async def main():
+    return CoursesList([Course('1 course','description of 1 course'),Course('2 course','description of 2 course')])
+
+@app.get('/api/get_profile_form')
+async def main():
+    return json.load(open('./TEST_form.json'))
---
+++ backend/test.py
@@ -0,0 +1,5 @@
+import requests as req
+r = req.request('post','http://localhost:8000/api/login')
+print(r.text)
+r = req.request('post','http://localhost:8000/api/get_courses')
+print(r.text)
