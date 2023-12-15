21024e40f39c9ca2dd0400a38e4c6eb1d0fe383d
Александр Мокичев <alex@MacBook-Pro-Aleksandr.local>
2023-12-15 14:48:59 +0300

    Hanging "cursor: pointer" on buttons and making logo clickable

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component{
     render(){
         return(
             <div className='Header'>
-                <div className='Name'>
+                <div className='Name' onClick={()=>this.props.go_to_menu()}>
                     Career Guider
                 </div>
                 {this.props.show_menu_button ? 
