891df0ef438f24752d19224d49d982e5e7647b18
Popov Dmitriy Ivanovich <PopovDmitriyStudy@yandex.ru>
2023-12-16 18:33:33 +0300

    Rewrite code to guess user profession

--- analys/analyze_user.py
+++ analys/analyze_user.py
@@ -1,23 +1,27 @@
-import pandas as pd
-import vk
 import time
-import nltk
 from nltk.corpus import stopwords
+import json
 import pymorphy3
-from analys.prof_info import file_mapping, user_data
-import unicodedata
+import time
+import pandas as pd
+from nltk.corpus import stopwords
+from analys.vk_api_resolver import VkApiResolver
 
-nltk.download('stopwords')
-morph = pymorphy3.MorphAnalyzer()
-
-CREATED_STOPWORDS_PATH = "analys/stopwords/custom_stopwords.txt"  
-PROF_KEYS_FOLDER = "analys/data/professions_keys/"
+CREATED_STOPWORDS_PATH = "analys/resourses/stopwords/custom_stopwords.txt"  
+PROF_KEY_WORDS_PATH = 'analys/resourses/professions_keywords.json'
+USER_DATA_OBJ_PATH = 'analys/resourses/user_data.json'
 
 with open(CREATED_STOPWORDS_PATH, "r", encoding="utf-8") as file:
     my_stopwords = set(file.read().splitlines())
 
-stop_words = set(stopwords.words("russian"))
+with open(PROF_KEY_WORDS_PATH,'r', encoding='utf-8') as keywords_file:
+    professions_keys = json.load(keywords_file)
 
+with open(USER_DATA_OBJ_PATH,'r', encoding='utf-8') as user_data_file:
+    user_data = json.load(user_data_file)
+
+stop_words = set(stopwords.words("russian"))
+morph = pymorphy3.MorphAnalyzer()
 
 class UserAnaliser(object):
     def __init__(self,id_user,token,user_choise) -> None:
@@ -25,16 +29,7 @@ class UserAnaliser(object):
         self._token = token
         self._user_choise = user_choise
         self._user_data = user_data.copy()
-
-    def _get_user_subscriptions(self):
-        vk_api = vk.API(self._token)
-        response = vk_api.users.getSubscriptions(user_id=self._id_user, v=5.92)
-        return response["groups"]["items"][:15]
-    
-    def _get_group_info(self,group_id):
-        vk_api = vk.API(self._token)
-        response = vk_api.groups.getById(group_id=group_id, fields="description", v=5.92)
-        return response[0]["name"], response[0]["description"]
+        self._vk_resolver = VkApiResolver(self._token,self._id_user)
 
     def clean_user_data(self,com):
         com.columns = ["name", "description"]
@@ -68,12 +63,13 @@ class UserAnaliser(object):
         return pd.DataFrame(keys)
 
     def get_user_keys(self):
-        k = self._get_user_subscriptions()
+        k = self._vk_resolver.get_user_subscriptions()
         group_info = {"name": [], "description": []}
 
         for sub in k:
             time.sleep(0.5)
-            tmp = self._get_group_info(sub)
+
+            tmp = self._vk_resolver.get_group_info(sub)
             name, description = tmp
             group_info["name"].append(name)
             group_info["description"].append(description)
@@ -84,20 +80,15 @@ class UserAnaliser(object):
         return pd.DataFrame(keys).drop_duplicates()
     
     def analyze_user(self,user_keywords):  
-        user_data = self._user_data
-        for category, keys in file_mapping.items():
+        for category, keys in professions_keys.items():
             # Нет смысла проходиться по категориям, которые неинтересны пользователю
             if category in self._user_choise  or len(self._user_choise)==0:
-                for file_name, profession in keys.items():
-                    # Правильно прописываем путь к папке с keys
-                    filename = PROF_KEYS_FOLDER + file_name
-                    filename = unicodedata.normalize("NFKC", filename)
-                    df = pd.read_csv(filename + "_keys.csv", header=None)
-                    normalized_words = df[0]
+                for profession, normalized_words in keys.items():
                     for i in user_keywords[0]:
                         if i in normalized_words.values:
                             self._user_data[profession] += 1
-        sorted_user_data = sorted(user_data.items(), key=lambda x: x[1], reverse=True)
+
+        sorted_user_data = sorted(self._user_data.items(), key=lambda x: x[1], reverse=True)
         #list типа [('Учитель', 56), ('Археолог', 37), ('Библиотекарь', 35)]
         return sorted_user_data[:3]
 
