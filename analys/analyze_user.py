d1729eb556009598a71292381be118620dd3c5e6
Fahnenfluchtige <kr4snovalya@gmail.com>
2023-12-16 12:38:04 +0300

    add json + edited some issues

--- analys/analyze_user.py
+++ analys/analyze_user.py
@@ -1,14 +1,11 @@
 import pandas as pd
 import vk
 import time
-from prof_info import file_mapping, user_data
 from get_info_by_public import get_groups_info
 from cleaning import clean_user_data, create_keys
-import unicodedata
+import json
 
-created_stopwords = (
-    "/home/evm/Hackaton/analys/stopwords/custom_stopwords.txt"  # Путь к созданным стоп-словам
-)
+created_stopwords = "stopwords/custom_stopwords.json"  # Путь к созданным стоп-словам
 
 
 def get_user_subscriptions(
@@ -26,63 +23,76 @@ def get_user_subscriptions(
 def get_user_keys(id_user, token):  # Получаем ключевые слова пользователя
     k = get_user_subscriptions(id_user, token)
 
-    filename = str(id_user) + "_piblics.csv"
-
     group_info = {"name": [], "description": []}
 
     for sub in k:
-        if get_groups_info(sub, token):
-            time.sleep(1) # А лучше 3
-            name, description = get_groups_info(sub, token)
+        group_info_result = get_groups_info(sub, token)
+
+        if group_info_result:
+            time.sleep(1)  # Лучше использовать 3 секунды
+            name, description = group_info_result
             group_info["name"].append(name)
             group_info["description"].append(description)
-        com = pd.DataFrame(group_info)
-        com.to_csv(filename, index=False)
+        else:
+            print("ошибка получения группы" + sub)
 
-    df = clean_user_data(filename)
+    com = pd.DataFrame(group_info)
+
+    df = clean_user_data(com)
 
     keys = create_keys(df, created_stopwords)
-    print(keys)
-    data = pd.DataFrame(keys)
-    data = data.drop_duplicates()
-    return data
+    if keys:
+        data = pd.DataFrame(keys)
+        data = data.drop_duplicates()
+        return data
+    else:
+        return None
 
 
 def analyze_user(
     user_choice, user_keywords, id_user
 ):  # Анализ ключевых слов пользователя
-    with open(str(id_user) + ".txt", "w", encoding="utf-8") as output_file:
-        for category, keys in file_mapping.items():
-            if (
-                category in user_choice
-            ):  # Нет смысла проходиться по категориям, которые неинтересны пользователю
-                for file_name, profession in keys.items():
-                    filename = (
-                        "/home/evm/Documents/professions_keys/" + file_name
-                    )  # Правильно прописываем путь к папке с keys
-                    filename = unicodedata.normalize("NFKC", filename)
-                    df = pd.read_csv(filename + "_keys.csv", header=None)
+    with open(
+        "professions_keys/keys_data.json",
+        "r",
+        encoding="utf-8",
+    ) as json_file:
+        key_data = json.load(json_file)
 
-                    normalized_words = df[0]
+    user_data = {}
 
-                    print(normalized_words.values)
-                    for i in user_keywords[0]:
-                        if i in normalized_words.values:
-                            user_data[profession] += 1
-                            print(i)
-                            output_file.write(i + "\n")
+    for category, professions in key_data.items():
+        if category in user_choice:
+            for profession, keywords in professions.items():
+                normalized_words = [word.lower() for word in keywords]
 
+                for i in user_keywords[0]:
+                    if i.lower() in normalized_words:
+                        if profession not in user_data:
+                            user_data[profession] = 0
+                        user_data[profession] += 1
 
-    sorted_user_data = sorted(user_data.items(), key=lambda x: x[1], reverse=True)
+    results = []
+    for i in range(3):
+        max_key = max(user_data, key=user_data.get)
+        max_value = user_data.pop(max_key)
+        results.append((max_key, max_value))
 
-    top_3 = sorted_user_data[:3] #list типа [('Учитель', 56), ('Археолог', 37), ('Библиотекарь', 35)]
-    for profession, value in top_3:
+    for profession, value in results:
         print(f"{profession}: {value}")
-    return top_3
+
+    if results:
+        return results
+    else:
+        print("ошибка получения результатов")
+        return None
 
 
 user_choice = ["Наука и образование"]  # Передаем массив с выбранными специальностями
 token = ""
-id_user = 425735901
+id_user = 175006893
 data = get_user_keys(id_user, token)
-k = analyze_user(user_choice, data, id_user)
+if data:
+    k = analyze_user(user_choice, data, id_user)
+else:
+    print("ошибка получения ключевых слов пользователя")
